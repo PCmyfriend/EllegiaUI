2d137bea465f8c2c3679885899a84a10b8a613fc
PCmyfriend <PCmyfriend@gmail.com>
2017-12-15 23:33:18 +0600

    Layout main content on the center of the screen.

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -15,6 +15,8 @@ import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Switch, Route } from 'react-router-dom';
 
+import { Grid, Row, Col } from 'react-flexbox-grid';
+
 import HomePage from '../../containers/HomePage/Loadable';
 import NotFoundPage from '../../containers/NotFoundPage/Loadable';
 import Header from '../../components/Header';
@@ -24,10 +26,16 @@ export default function App() {
     <MuiThemeProvider>
       <div>
         <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route component={NotFoundPage} />
-        </Switch>
+        <Grid fluid />
+        <Row center="xs">
+          <Col xs={6}>
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route component={NotFoundPage} />
+            </Switch>
+          </Col>
+        </Row>
+        <Grid />
       </div>
     </MuiThemeProvider>
   );
