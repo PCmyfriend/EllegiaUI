1bf1a48ddf8ca531e76b35e5d23d3c47dbd8f018
PCmyfriend <PCmyfriend@gmail.com>
2017-12-15 21:23:05 +0600

    Add russian locale to the project.

--- app/i18n.js
+++ app/i18n.js
@@ -6,15 +6,19 @@
  */
 import { addLocaleData } from 'react-intl';
 import enLocaleData from 'react-intl/locale-data/en';
+import ruLocaleData from 'react-intl/locale-data/ru';
 
 import { DEFAULT_LOCALE } from './containers/App/constants'; // eslint-disable-line
 import enTranslationMessages from './translations/en.json';
+import ruTransactionMessages from './translations/ru.json';
 
 export const appLocales = [
   'en',
+  'ru',
 ];
 
 addLocaleData(enLocaleData);
+addLocaleData(ruLocaleData);
 
 export const formatTranslationMessages = (locale, messages) => {
   const defaultFormattedMessages = locale !== DEFAULT_LOCALE
@@ -31,4 +35,5 @@ export const formatTranslationMessages = (locale, messages) => {
 
 export const translationMessages = {
   en: formatTranslationMessages('en', enTranslationMessages),
+  ru: formatTranslationMessages('ru', ruTransactionMessages),
 };
